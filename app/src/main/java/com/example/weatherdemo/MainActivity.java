f58ec17816548867fc6cc0d4470234c3d6fa7f17
3218005445 <1649364691@qqcom>
2021-05-20 16:51:40 +0800

    结合使用ViewPager2、Fragment、TabLayout

--- app/src/main/java/com/example/weatherdemo/MainActivity.java
+++ app/src/main/java/com/example/weatherdemo/MainActivity.java
@@ -2,22 +2,23 @@ package com.example.weatherdemo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.weatherdemo.Utils.JsonUtils;
+import com.example.weatherdemo.Utils.MyAdapter;
 import com.example.weatherdemo.entities.CityInfo;
-
-import org.jetbrains.annotations.NotNull;
+import com.example.weatherdemo.fragments.DetailsFragment;
+import com.example.weatherdemo.fragments.HomeFragment;
+import com.example.weatherdemo.fragments.SettingsFragment;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
@@ -26,9 +27,11 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager2 viewPager2 = null;
+    private ViewPager2 myPager2;
+    private TabLayout myTab;
 
-    private ArrayList<View> viewContainter = new ArrayList<View>();
+    List<String> titles = new ArrayList<>();
+    List<Fragment> fragments = new ArrayList<>();
 
     private CityInfo cityInfo;
 
@@ -44,50 +47,39 @@ public class MainActivity extends AppCompatActivity {
      * 初始化组件
      */
     private void initView() {
-        viewPager2 = findViewById(R.id.vp_show);
 
-        View homeView = LayoutInflater.from(this).inflate(R.layout.activity_home_page, null);
-        View detailsView = LayoutInflater.from(this).inflate(R.layout.activity_details_page, null);
+        myPager2 = findViewById(R.id.vp2_show);
+        myTab = findViewById(R.id.tb_01);
 
-        //  viewPager2添加view
-        viewContainter.add(homeView);
-        viewContainter.add(detailsView);
+        //添加标题
+        titles.add("首页");
+        titles.add("详情页");
+        titles.add("设置页");
 
-        //设置Adapter
-        viewPager2.setAdapter(new MyPagerAdapters());
+        //添加Fragment
+        fragments.add(new HomeFragment());
+        fragments.add(new DetailsFragment());
+        fragments.add(new SettingsFragment());
+
+        //实例化设配器
+        MyAdapter myAdapter = new MyAdapter(getSupportFragmentManager(), getLifecycle(), fragments);
+
+        //设置适配器
+        myPager2.setAdapter(myAdapter);
+
+        //TabLayout和Viewpager2进行关联
+        new TabLayoutMediator(myTab, myPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(titles.get(position));
+            }
+        }).attach();
     }
+
 
     /**
-     * ViewPager2数据适配器
+     * 请求城市网络资源
      */
-    class MyPagerAdapters extends PagerAdapter {
-
-        //返回可以滑动的VIew的个数
-        @Override
-        public int getCount() {
-            return viewContainter.size();
-        }
-
-        //滑动切换的时候销毁当前的组件
-        @Override
-        public void destroyItem(ViewGroup container, int position,
-                                Object object) {
-            ((ViewPager2) container).removeView(viewContainter.get(position));
-        }
-
-        //将当前视图添加到container中并返回当前View视图
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            ((ViewPager2) container).addView(viewContainter.get(position));
-            return viewContainter.get(position);
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-    }
-
     private void initCityInfoFromJson() {
         //请求网络资源
         Handler mainHandler = new Handler(getMainLooper());
---
+++ app/src/main/java/com/example/weatherdemo/Utils/MyAdapter.java
@@ -0,0 +1,33 @@
+package com.example.weatherdemo.Utils;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class MyAdapter extends FragmentStateAdapter {
+
+    List<Fragment> fragments;
+
+    public MyAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, List<Fragment> fragments) {
+        super(fragmentManager, lifecycle);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
